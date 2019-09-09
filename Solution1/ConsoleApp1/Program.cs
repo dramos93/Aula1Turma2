dd014a535f7f75c2099fa0f419e2ce807dc62cb6
Sss41 <silvana.souza63@gmail.com>
2019-09-09 15:32:59 -0300

    Commit Inicial

---
+++ Solution1/ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Teste!");//Escreve na tela o que está dentro do parênteses
+            Console.ReadKey();//Serve para sair da tela apenas depois do enter
+        }
+    }
+}
