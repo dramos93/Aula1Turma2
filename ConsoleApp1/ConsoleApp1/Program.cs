7d5c67374c01e0fbacf686b72fe8c7e310e93bab
Daniel Silva Ramos <daniel.silvaramos.93@outlook.com>
2019-09-10 17:22:50 -0300

    Primeiras Impressões da Programação

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Texto informativo para consultar o livro pelo
+            //número de registro do sistema
+            Console.WriteLine("Informe o livro a ser consultado: ");
+            //Parte do código que recebe as informações
+            //de registro do livro e coloca na variável
+            //numeroDoLivro
+            var numeroDoLivro = Console.ReadLine();
+            //Aqui realizo a comparação das informações
+            //do livro informado com o que tenho disponível em estoque
+            if (numeroDoLivro == "123456")
+            {
+                Console.WriteLine("Livro Indisponível");
+                Console.ReadKey();
+                //Finaliza o método
+                return;
+            }
+            else
+            {
+                Console.WriteLine("Deseja alocar o Livro? (1) Sim (2) Não");
+                var resposta = Console.ReadLine();
+                if(resposta=="1")
+                {
+                    Console.WriteLine("Livro Alocado");
+                    Console.ReadKey();
+                    return;
+
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
