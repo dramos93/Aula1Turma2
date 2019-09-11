d3c7f6c75ce36fab3fdda5ce004801cb89467b32
Daniel Silva Ramos <daniel.silvaramos.93@outlook.com>
2019-09-11 16:37:18 -0300

    até aqui foi array

---
+++ MatrizArray/MatrizArray/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatrizArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] arrayLinear = new string[3] { "Felipe", "Giomar", "Euricledson" };//new= Nova estância, para ser acessada quando quiser
+
+            List<string> listaLinear = new List<string>
+            {
+                "Felipe",
+                "Giomar",
+                    "Euricledson"
+            };
+
+            Console.WriteLine(arrayLinear[1]);
+            Console.WriteLine(listaLinear[1]);
+            Console.ReadKey();//Key lê uma tecla, para pegar qual tecla, coloca-sse .keychar.ToString()
+        }
+    }
+}
