1b093baf0acb80b1e4790e31e0d40fafa3481724
Daniel Silva Ramos <daniel.silvaramos.93@outlook.com>
2019-09-13 13:59:19 -0300

    Consegui colocar hora e também usar condição com horário

---
+++ AppCicle/TimeNowShow/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TimeNowShow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                //Console.Clear();
+                //Console.WriteLine($"Hora Atual: {DateTime.Now.ToString("dd:MM:HH:mm:ss:fffff")}");//Aqui eu coloco data e hora atual
+                //Thread.Sleep(250);
+                if (DateTime.Now.ToString("ss") == "03" || DateTime.Now.ToString("ss") == "13" )
+                {
+                    Console.WriteLine($"Hora Atual: {DateTime.Now.ToString("dd:MM:HH:mm:ss:fffff")}");//Aqui eu coloco data e hora atual
+                }
+            }
+        }
+    }
+}
