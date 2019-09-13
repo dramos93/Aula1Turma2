7302ff439e5b04187330c63387a0867192a6db49
Daniel Silva Ramos <daniel.silvaramos.93@outlook.com>
2019-09-13 10:51:41 -0300

    salveeeei

---
+++ AppCicle/CicloWhile/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CicloWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ShowIntAppText();
+
+            while (MetodoPergunta() == 1)//a resposta é string, tem que colocar o valor em parenteses
+            {
+
+            }
+
+            Console.ReadKey();
+        }
+        /// <summary>
+        /// Método para mostrar apenas a mensagem
+        /// </summary>
+        private static void ShowIntAppText()// Void não retorna um resultado
+        {
+            Console.WriteLine("-------------------");
+            Console.WriteLine("--Seja Bem Vindo!--");
+            Console.WriteLine("-------------------");
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        private static int MetodoPergunta()
+        {
+            Console.WriteLine("Vamos Conversar? 1-Sim 2-Não");
+            return int.Parse(Console.ReadKey().KeyChar.ToString());//KeyChar é a letra que está escrita
+
+        }
+    }
+}
---
+++ AppCicle/PodeBeber/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PodeBeber
+{
+    class Program
+    {
+        public static int idade;
+        static void Main(string[] args)
+        {
+
+            ShowIntAppText();
+            idade = PerguntaIdade();
+            RespostaSim();
+            RespostaNao();
+
+
+            //if (PerguntaIdade() >= 18)//a resposta é string, tem que colocar o valor em parenteses
+            //{
+            //    Console.WriteLine("Você pode Beber!");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Você não pode Beber!");
+            //}
+
+            Console.ReadKey();
+        }
+        /// <summary>
+        /// Método para mostrar apenas a mensagem
+        /// </summary>
+        private static void ShowIntAppText()// Void não retorna um resultado
+        {
+            Console.WriteLine("-------------------");
+            Console.WriteLine("--Seja Bem Vindo!--");
+            Console.WriteLine("-------------------");
+            Console.WriteLine("Digite sua idade: ");
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        private static int PerguntaIdade()
+        {
+            return int.Parse(Console.ReadLine());//KeyChar é a letra que está escrita
+        }
+        private static void RespostaSim()
+        {
+            if (idade >= 18) 
+            {
+                Console.WriteLine("Você pode Beber!");
+            }
+        }
+        private static void RespostaNao()
+        {
+            if (idade < 18)
+            {
+                Console.WriteLine("Você não pode Beber!");
+            }
+        }
+
+    }
+}
