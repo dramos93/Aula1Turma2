ce81f8b06745fd267da1d089fbfa52c24a1e4760
Daniel Silva Ramos <daniel.silvaramos.93@outlook.com>
2019-09-16 14:25:21 -0300

    Aprendemos .split

---
+++ Aulas/Dia06_2_IniciandoForeach/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dia06_2_IniciandoForeach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ForeachComSplit();
+        }
+        /// <summary>
+        /// Método que mostra como podemos utilizae o foreach para andar sobre
+        /// uma array de caracteres, ou seja, um texto ou palabra, etc..
+        /// </summary>
+        private static void InciandoForeach01()
+
+        {
+            Console.WriteLine("Informar o texto: ");
+            var conteudoTexto = Console.ReadLine();
+            foreach (var item in conteudoTexto) //
+            {
+                if (item == 'e')
+                    continue;//Aqui ele pula o que está abaixo dele.
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+        private static void ForeachComSplit()
+        {
+            var conteudoTexto = "Aqui vou colocar meu nome Felipe para realizar a busca";
+            Console.WriteLine("Informe a palavra para realizar a busca> ");
+            var palavra = Console.ReadLine();
+            var conteudoTextoSplit = conteudoTexto.Split(' ');// Quebra a informação por espaço.
+            foreach (var item in conteudoTextoSplit)
+            {
+                if (palavra==item)
+                    Console.WriteLine("Palavra encontrada com sucesso!");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Aulas/Texte/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Texte
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string phrase = "The quick brown fox jumps over the lazy dog.";
+            //string[] words = phrase.Split(' ');
+            foreach (var word in phrase)
+            {
+                System.Console.WriteLine($"<{word}>");
+            }
+        }
+    }
+}
