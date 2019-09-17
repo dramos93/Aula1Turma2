6ce1b8b2d1f6d07d5b042829b8026f7da8ddb381
Daniel Silva Ramos <daniel.silvaramos.93@outlook.com>
2019-09-17 17:23:58 -0300

    Atividade de Louco começada

---
+++ Aulas/Dia07_Crazy_Project/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dia07_Crazy_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IniciarPrograma();
+        }
+        public static void IniciarPrograma()
+        {
+            MetodoArvore();
+            MetodoCalculoArea();
+            MetodoListaCerveja();
+            MetodoListaCarro();
+        }
+        private static void MetodoArvore()
+        {
+
+        }private static void MetodoCalculoArea()
+        {
+            Console.WriteLine("--- Cálculo de Área de um Quadrado ---");
+            Console.WriteLine("Digite o valor de um do lado: ");
+            var lado = int.Parse(Console.ReadLine());
+            lado = lado * lado;
+            Console.WriteLine($"\nO valor da área do quadrado é de : {lado} metros");
+            Console.ReadKey();
+            Console.Clear();
+        }private static void MetodoListaCerveja()
+        {
+            var cervejas = new List<string>()
+            { "Brahma", "Skol", "Antartica", "Patagonia", "Original"};
+            cervejas.ForEach(nomes => Console.WriteLine(nomes));
+            Console.ReadKey();
+            Console.Clear();
+        }
+        private static void MetodoListaCarro()
+        {
+            var carros = new List<string>()
+            { "BMW", "Fiat", "Ford", "Chevrolet", "Kia", "volkswagen", "Mercedes-Benz"};
+            carros.ForEach(nomes => Console.WriteLine(nomes));
+            Console.ReadKey();
+            Console.Clear();
+        }
+
+    }
+}
