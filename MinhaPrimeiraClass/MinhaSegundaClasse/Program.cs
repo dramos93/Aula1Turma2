f0da41010e30b75b862a9cb402ec8630c3932158
Daniel Silva Ramos <daniel.silvaramos.93@outlook.com>
2019-09-18 17:27:12 -0300

    Salvei, minha primeira Class

---
+++ MinhaPrimeiraClass/MinhaSegundaClasse/Program.cs
@@ -0,0 +1,24 @@
+﻿using MinhaSegundaClasse.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace MinhaSegundaClasse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var listaDeFrutas = new List<CestaDeFrutas>();
+            listaDeFrutas.Add(new CestaDeFrutas()
+            {
+                Nome = "banana",Quantidade =5
+            });
+            listaDeFrutas.ForEach(i => Console.WriteLine($"Nome {i.Nome} Quantidade {i.Quantidade}"));
+            Console.ReadKey();
+        }
+    }
+}
