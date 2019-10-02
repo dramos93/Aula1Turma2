2d7d46e3d98fbac9466d9a294f3fd1a57422e376
Daniel Silva Ramos <daniel.silvaramos.93@outlook.com>
2019-10-02 17:22:07 -0300

    Salvando meu exercício, Script

---
+++ Semana 4 - 30 a 04-10/Script/ExercícioFuncionarios.sql
@@ -0,0 +1,32 @@
+﻿create database SistemaRH
+GO
+use SistemaRH
+GO
+create table [dbo].[Salarios]
+(
+	[Id]				INT  PRIMARY KEY IDENTITY(1,1),
+	[Funcionario]		INT					NOT NULL,
+	[Valor]				MONEY			DEFAULT 0 NOT NULL UNIQUE,
+    [Ativo]				BIT				DEFAULT ((1)) NULL,
+    [UsuarioCriacao]	INT				DEFAULT ((0)) NULL,
+    [UsuarioAlteracao]	INT				DEFAULT ((0)) NULL,
+    [DataCriacao]		DATETIME		DEFAULT (getdate()) NULL,
+    [DataAlteracao]		DATETIME		DEFAULT (getdate()) NULL,
+)
+GO
+create table [dbo].[Funcionarios]
+(
+	[Id]				INT  PRIMARY KEY IDENTITY(1,1),
+	[Nome]				NVARCHAR(30)	NOT NULL,
+    [DataInicio]		DATETIME		DEFAULT (getdate()) NULL,
+    [DataSaida]			DATETIME		DEFAULT () NULL,
+    [Ativo]				BIT				DEFAULT ((1)) NULL,
+    [UsuarioCriacao]	INT				DEFAULT ((0)) NULL,
+    [UsuarioAlteracao]	INT				DEFAULT ((0)) NULL,
+    [DataCriacao]		DATETIME		DEFAULT (getdate()) NULL,
+    [DataAlteracao]		DATETIME		DEFAULT (getdate()) NULL,
+)
+GO
+INSERT INTO Funcionarios(Nome,DataInicio) VALUES ('Daniel Silva Ramos','06/02/2018')
+INSERT INTO Funcionarios(Nome,DataInicio) VALUES ('Suzana Paulista','12/02/2018')
+INSERT INTO Funcionarios(Nome,DataInicio) VALUES ('Magda Ramos','04/07/2018')
---
+++ Semana 4 - 30 a 04-10/Script/Filtro do Filtro.sql
@@ -0,0 +1,8 @@
+﻿SELECT * from Clientes;
+select * from Pedidos;
+
+select cli.* from Clientes cli inner join Pedidos ped on cli.Id = ped.CLIENTEID 
+where ped.NUMERO in ('s003','s004','s006')
+
+Select * from pedidos ped where ped.CLIENTEID in (select cli.Id from Clientes cli inner join Pedidos ped on cli.id = ped.CLIENTEID where ped.NUMERO in ('s003','s004','s006'))
+order by ped.VALOR desc
