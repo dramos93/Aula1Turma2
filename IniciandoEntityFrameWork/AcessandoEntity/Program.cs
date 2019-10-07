3f6332db53d5d76a9eac77f6634a0fda343f45f6
Daniel Silva Ramos <daniel.silvaramos.93@outlook.com>
2019-10-07 15:04:47 -0300

    .

---
+++ IniciandoEntityFrameWork/AcessandoEntity/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EntityClass.Controller;
+using EntityClass.Model;
+
+namespace AcessandoEntity
+{
+    class Program
+    {
+        static PessoaController pessoa = new PessoaController();
+        static void Main(string[] args)
+        {
+            pessoa.AddPessoa(new Pessoa() { Nome = "Abigail"});//Inser um nome no banco de Dados
+            Console.WriteLine();
+            pessoa.GetPessoas().ToList<Pessoa>().//btemos a lista de Pessoas
+                ForEach(x => Console.WriteLine(x.Nome));
+            Console.ReadKey();
+            Console.ReadKey();
+            Console.ReadKey();
+            
+        }
+    }
+}
---
+++ IniciandoEntityFrameWork/EntityClass/Model/Pessoa.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityClass.Model
+{
+    public class Pessoa
+    {   
+        [Key]//Aqui temos um anotação que indica que nossa chave é o campo logo abaixo da nossa anotação
+             //e a palavra chabe [Key] significa 
+        public int Id { get; set; }
+        public String Nome { get; set; }
+
+    }
+}
