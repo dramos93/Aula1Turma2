36f4f82994b9209b7b740edf430ec20884b447ac
Daniel Silva Ramos <daniel.ramos@hbsis.com.br>
2019-10-07 06:29:32 -0300

    Exerccio Feito

---
+++ Semana 4 - 30 a 04-10/Script/Floricultura.sql
@@ -0,0 +1,29 @@
+﻿
+/*1 - Criar uma base de dados chamada FloriculturaDB
+	1.1 - A base deve conter as seguintes tabelas e seus campos
+		1.1.1 - Flores
+			1.1.1.1 - Id,Nome,Quantidade
+	1.2 - Devemos inserir ao menos 5 flores dentro de nossa tabelas*/
+create database FloriculturaDB
+go
+Use FloriculturaDB 
+Go
+Create Table Flores
+(
+	[Id]			int				identity(1,1) ,
+	[Nome]			nVarChar(30)	not null		unique,
+	[Quantidade]	int				default 0
+)
+insert into Flores (Nome,Quantidade)values('Rosa',10)
+insert into Flores (Nome,Quantidade)values('Bromélia',67)
+insert into Flores (Nome,Quantidade)values('Cravo',35)
+insert into Flores (Nome,Quantidade)values('Mosquitinho',357)
+insert into Flores (Nome,Quantidade)values('Tulipa',27)
+GO
+--1.3 - Devemos ter uma seleção ordenada das flores, da que contem a maior quantidade para a menor
+Select * from Flores order by Quantidade desc
+GO
+--1.3 - Devemos retornar uma seleção com a quantidade total de flores que temos em nossa tabela
+Select Distinct Sum(Flores.Quantidade) as 'Total de Flores' from Flores 
+Go
+
