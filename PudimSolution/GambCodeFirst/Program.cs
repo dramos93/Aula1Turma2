ac9a0bfd2ae24309b030d2db0145849ddd1bc158
Daniel Silva Ramos <daniel.silvaramos.93@outlook.com>
2019-10-09 08:20:19 -0300

    CodeFirst

---
+++ PudimSolution/GambCodeFirst/Program.cs
@@ -0,0 +1,22 @@
+﻿using CodeFirtTeste.Controller;
+using CodeFirtTeste.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GambCodeFirst
+{
+    class Program
+    {
+        static CervejaController cerveja = new CervejaController();
+        static void Main(string[] args)
+        {
+            
+            cerveja.AddCerveja(new Cerveja { Nome = "Cerveja Skol" });
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ RevisaoEntity/RevisandoEntity/Model/CervejaContextDB.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevisandoEntity.Model
+{
+    public class CervejaContextDB : DbContext       //Usa-se ctrl + "." para faezr o using; Herdamos nossa classe DbContext que faz toda a parte do banco de dados
+    {
+        public DbSet<Cerveja> Cervejas { get; set; } //Definimos nossa tabela dentro do banco de dados, se não for colocado DbSet não vira uma tabela
+    }
+}
