07450295732c9cc3f5e2b7c152b22318d5e54f67
Daniel Silva Ramos <daniel.silvaramos.93@outlook.com>
2019-10-10 17:21:53 -0300

    Salvando, Atividade 01, Nomes Concluída!

---
+++ ListarNomes/Nomes/Controller/NomesController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nomes.Model;
+
+namespace Nomes.Controller
+{
+    public class NomesController
+    {
+        public NomesContext GetNomesContext = new NomesContext();
+
+        /// <summary>
+        /// Método Responsável por enviar todos os nomes
+        /// </summary>
+        /// <returns>Envia a lista de Nomes</returns>
+        public IQueryable<Name> ListarNomes()
+        {
+            return GetNomesContext.GetNomes;
+        }
+
+        public bool AdicionarNome(Name item)
+        {
+            if (string.IsNullOrWhiteSpace(item.Nome))
+                return false;
+            else
+            {
+                GetNomesContext.GetNomes.Add(item);
+                GetNomesContext.SaveChanges();
+                return true;
+            }
+        }
+    }
+}
---
+++ ListarNomes/NomesInterface/Program.cs
@@ -0,0 +1,91 @@
+﻿using Nomes.Controller;
+using Nomes.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NomesInterface
+{
+    class Program
+    {
+        static NomesController NomesController = new NomesController();
+        static void Main(string[] args)
+        {
+                int n = int.MinValue;//a variável opcao vai começar com um valor que é maior que zero, pode ser qualquer valor
+
+            while (n != 0)
+            {
+                Console.Clear();
+                Console.WriteLine("--------------MENU---------------");
+                Console.WriteLine("1 - Adicionar Nome");
+                Console.WriteLine("2 - Mostrar Nomes");
+                Console.WriteLine("0 - Sair");
+                Console.WriteLine("---------------------------------");
+
+                Console.Write("\nEscolha uma das opções Acima: ");
+
+                var opcao = int.TryParse(Console.ReadLine(), out n);
+                if (opcao)
+                {
+                    Console.Clear();
+
+                    switch (n)
+                    {
+                        case 1:
+                            AdicionarNome();
+                            break;
+                        case 2:
+                            MostrarNome();
+                            break;
+                        case 0:;
+                            break;
+                        default:
+                            Erro();
+                            break;
+                    }
+                }
+                else
+                {
+                    n = int.MinValue;
+                    Erro();
+                }
+            }
+        }
+
+        private static void Erro()
+        {
+            Console.WriteLine("\nOpção Inválida, Tente Novamente!");
+            Console.Write("\nClique em Qualquer Tecla Para Voltar ao Menu...");
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Método que mostra os nome que tem no Banco de Dados
+        /// </summary>
+        private static void MostrarNome()
+        {
+            NomesController.ListarNomes().ToList().ForEach(i => Console.WriteLine($"Id: {i.Id} | Nome: {i.Nome}"));
+            Console.Write("\nClique em Qualquer Tecla Para Voltar ao Menu...");
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Método que Adiciona um nome ao meu Banco de Dados
+        /// </summary>
+        private static void AdicionarNome()
+        {
+            Console.WriteLine("Digite um novo Nome: ");
+            var nome = Console.ReadLine();
+
+            var retorno = NomesController.AdicionarNome(new Name{ Nome = nome });
+            if (retorno)
+                Console.WriteLine("\nNome Adicionado com Sucesso");
+            else
+                Console.WriteLine("\nErro ao Adicionar Nome, Tente Novamente");
+            Console.Write("\nClique em Qualquer Tecla Para Voltar ao Menu...");
+            Console.ReadKey();
+        }
+    }
+}
