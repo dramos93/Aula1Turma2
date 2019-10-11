8905c6f1fdb83fded2a4a8af9e41f61e749655be
Daniel Silva Ramos <daniel.ramos@hbsis.com.br>
2019-10-11 06:21:41 -0300

    Atividade 2 Iniciada

---
+++ Floricultura1/ClassFloricultura/Controller/FloresController.cs
@@ -0,0 +1,36 @@
+﻿using ClassFloricultura.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassFloricultura.Controller
+{
+    public class FloresController
+    {
+        public FloresContext floresController = new FloresContext();
+
+        /// <summary>
+        /// Método que Retorna Lista de Flores
+        /// </summary>
+        /// <returns>Lista de Flores</returns>
+        public IQueryable<Flores> GettFlores()
+        {
+            return floresController.Flor;
+        }
+
+        public bool AddFlor(Flores item)
+        {
+            var retornoNome = (string.IsNullOrWhiteSpace(item.Nome));
+            if (!retornoNome)
+                return false;
+            else
+            {
+                floresController.Flor.Add(item);
+                floresController.SaveChanges();
+                return true;
+            }
+        }
+    }
+}
