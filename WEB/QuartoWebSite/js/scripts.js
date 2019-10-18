7853a9d19ec4b9be3fa561c12a8889c458811c2d
Daniel Silva Ramos <daniel.silvaramos.93@outlook.com>
2019-10-18 11:56:58 -0300

    Site do Cachorro Iniciado

--- WEB/QuartoWebSite/js/scripts.js
+++ WEB/QuartoWebSite/js/scripts.js
@@ -1,64 +1,28 @@
 $(document).ready(
-    function () {
-        $('input[id="valor1"]').mask('#.##0,00', { reverse: true });
-        $('input[id="valor2"]').mask('#.##0,00', { reverse: true});
+    function myfunction() {
+        function Dog(Nome, Idade, Raça, Coloracao) {
+            this.Nome = Nome;
+            this.Idade = Idade;
+            this.Raça = Raca;
+            this.Coloracao= Coloracao;
+        }
+        $('button[name="btnboasvindas"]').click(function () {
+            debugger
+            var nomeUsuario = $('input[name="nomeUsuario"]').val();
+            //alert("Bem vindo " + nomeUsuario + ", ao nosso sistema de cervejaria.");
+            $('span[name="usuariotext"]').text(nomeUsuario);
+
+
+        });
+
+
+
+
+
+
+
+
+
+
+
     });
-
-
-$('input[id="OK"]').click(
-    function Converter() {
-
-        var valor1 = $('input[id="valor1"]').val();//valor em Reais
-        var valor2 = $('input[id="valor2"]').val();//Valor em Dólares
-
-
-        valor1 = valor1.replace(".", "");
-        valor1 = valor1.replace(",", ".");
-        valor1 = valor1.replace("R$ ", "");
-
-        valor2 = valor2.replace(".", "");
-        valor2 = valor2.replace(",", ".");
-        valor2 = valor2.replace("$ ", "");
-
-        var resultado;
-        if (valor1 != "" ^  valor2 != "") {
-            if (valor1 != "") {
-                resultado = valor1 * 4.16;
-                var indexPonto = String(resultado).indexOf(".");
-                var resultadoFormatado = String(resultado).substring(0, indexPonto + 3)
-                resultadoFormatado = resultadoFormatado.replace(".", ",");
-                $('input[id="valor2"]').val("$ " + resultadoFormatado);
-            }
-            if (valor2 != "") {
-                resultado = valor2 / 4.16;
-                indexPonto = String(resultado).indexOf(".");
-                resultadoFormatado = String(resultado).substring(0, indexPonto + 3)
-                resultadoFormatado = resultadoFormatado.replace(".", ",")
-                $('input[id="valor1"]').val("R$ " + resultadoFormatado);
-            }
-
-        }
-        //if (valor2 != "") {
-        //    alert("valor 2 recebido");
-        //}
-        else {
-            alert("Campo Digitado Incorretamente!");
-        }
-    }
-);
-$('input[id="Limpar"]').click(
-    function () {
-        $('input[Id="valor1"]').val("");
-        $('input[Id="valor2"]').val("");
-    })
-
-
-
-//Abaixo não são utilizados...
-function atualizaData() {
-    setInterval(function () { $('span[name="data_atual"]').text(Date()); });
-}
-function mostrarNome() {
-    var nome = $('input[id="nome"]').val();
-    alert("Seja bem vindo, " + nome + "!");
-}
