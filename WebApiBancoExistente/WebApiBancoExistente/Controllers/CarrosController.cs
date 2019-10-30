7e6b801340347684f63ffb592263b88a41c7a301
Daniel Silva Ramos <daniel.silvaramos.93@outlook.com>
2019-10-30 09:19:29 -0300

    Feito: 4 - Vamos trazer somente a resolução dos nomes  para as demais tabelas como foi aplicado para a nossa tabela de carros.
    5 - Vamos realizar a relação da nossa tabela de carros com a de usuários, vamos retornar uma consulta aonde informamos o id do carro
    e retornamos o usuário que cadastrou aquele veículo.

--- WebApiBancoExistente/WebApiBancoExistente/Controllers/CarrosController.cs
+++ WebApiBancoExistente/WebApiBancoExistente/Controllers/CarrosController.cs
@@ -17,34 +17,68 @@ namespace WebApiBancoExistente.Controllers
         private Carroes db = new Carroes();
 
         [HttpGet]
-        [Route("Api/Carroes/CustomQuery")]
-        public object CustomCarroQuery()
+        [Route("Api/Carroes/CustomQueryCarros")]
+        public object CustomQueryCarros()
         {
             var listaDeCarros = db.Carros.ToList();
 
-            var retornoCarros = from carros in listaDeCarros select new {NomeCarro = carros.Modelo, CarrosId = carros.Id };
+            var retornoCarros = from carros in listaDeCarros 
+                                select new {NomeCarro = carros.Modelo, CarrosId = carros.Id };
+
+            return retornoCarros;
+        }
+
+        [HttpGet]
+        [Route("Api/Carroes/CustomQueryMarcas")]
+        public object CustomQueryMarcas()
+        {
+            var listaDeMarcas = db.Marcas.ToList();
+
+            var retornoCarros = from Marcas in listaDeMarcas 
+                                select new { NomeMarca = Marcas.Nome, MarcasId = Marcas.Id };
+
+            return retornoCarros;
+        }
+
+        [HttpGet]
+        [Route("Api/Carroes/CustomQueryUsuarios")]
+        public object CustomQueryUsuarios()
+        {
+            var listaDeUsuarios = db.Usuarios.ToList();
+
+            var retornoCarros = from Usuarios in listaDeUsuarios 
+                                select new { NomeUsuario = Usuarios.Usuario, UsuaruiosId = Usuarios.Id };
 
             return retornoCarros;
         }
 
         [HttpGet]
         [Route("Api/Carroes/CarrosComMarcas")]
-        public object customCarrosOnMarcas()
+        public object CustomCarrosOnMarcas()
         {
             var listaCarros = db.Carros.ToList();
             var listaMarcas = db.Marcas.ToList();
-            var conteudoRetorno = from mar in listaMarcas join car in listaCarros on mar.Id equals car.Marca select new { CarroId = car.Id, CarroNome = car.Modelo, MarcaId = mar.Id, MarcaNome = mar.Nome };
+            var conteudoRetorno = from mar in listaMarcas
+                                  join car in listaCarros on mar.Id 
+                                  equals car.Marca 
+                                  select new { CarroId = car.Id, CarroNome = car.Modelo, MarcaId = mar.Id, MarcaNome = mar.Nome };
 
             return conteudoRetorno;
         }
 
+        [HttpGet]
+        [Route("Api/Carroes/CarrosComUsuarios")]
+        public object CarrosComUsuarios()
+        {
+            var listaCarros = db.Carros.ToList();
+            var listaUsuarios = db.Usuarios.ToList();
+            var conteudoRetorno = from Usu in listaUsuarios 
+                                  join car in listaCarros on Usu.Id 
+                                  equals car.Marca 
+                                  select new { CarroId = car.Id, CarroNome = car.Modelo, IdUsuario = Usu.Id, NomeUsuario= Usu.Usuario };
 
-
-
-
-
-
-
+            return conteudoRetorno;
+        }
 
 
 
