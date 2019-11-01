fe13e246aa99969226eca70dbdb0064a6153ee91
Daniel Silva Ramos <daniel.silvaramos.93@outlook.com>
2019-11-01 17:29:01 -0300

    Começamos .net Core

---
+++ FirstCoreWebApi/FirstCoreWebApi/Models/FirstCodeContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FirstCoreWebApi.Models;
+
+namespace FirstCoreWebApi.Models
+{
+    public class FirstCodeContext : DbContext
+    {
+        public FirstCodeContext(DbContextOptions<FirstCodeContext> options) : base (options)
+        {
+
+        }
+        public DbSet<FirstCoreWebApi.Models.Carro> Carro { get; set; }
+    }
+}
