aaba8afc879660e3b74a996edd617011f7b92681
Daniel Silva Ramos <daniel.silvaramos.93@outlook.com>
2019-11-04 14:10:49 -0300

    Finalizado:
    1.1 - Nossa primeira solução envolve criar uma api aonde vamos informar a densidade de um objeto e seu volume total,
      após obtermos essa informação conseguimos calcular seu peso de acordo com a gravidade padrão do nosso planeta.

---
+++ VolumeWebApiCore/VolumeWebApiCore/Models/Objeto.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VolumeWebApiCore.Models
+{
+    public class Objeto
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string NomeObjeto { get; set; }
+        public double Densidade { get; set; }
+        public double VolumeTotal{ get; set; }
+
+        public double Peso { get; set; }
+        
+
+    }
+}
---
+++ VolumeWebApiCore/VolumeWebApiCore/Models/ObjetoContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VolumeWebApiCore.Models
+{
+    public class ObjetoContext : DbContext
+    {
+        public ObjetoContext(DbContextOptions<ObjetoContext> options)
+            : base(options)
+        {
+
+        }
+        public DbSet<Objeto> Objetos { get; set; }
+    }
+}
