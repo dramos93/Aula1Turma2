d4065e512168f313741144071874b546c5784867
Daniel Silva Ramos <daniel.silvaramos.93@outlook.com>
2019-11-05 15:58:35 -0300

    Feito:
    2 - Vamos criar uma aplicação completa agora com api net core aonde vamos ter a interface em html conectando por js com nossa api e listando as informações, podemos criar o banco de dados primeiramente para após isso construir a api.
    
     2.2 - Vamos ter uma tabela de usuário, com os respectivos campos: Nome,Login,Senha,Email com validação para o campo de e-mail.

---
+++ ProjetFinalWebApiNetCore/ProjetFinalWebApiNetCore/Models/ProjetoFinalContext.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace ProjetFinalWebApiNetCore.Models
+{
+    public partial class ProjetoFinalContext : DbContext
+    {
+        public ProjetoFinalContext()
+        {
+        }
+
+        public ProjetoFinalContext(DbContextOptions<ProjetoFinalContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Usuarios> Usuarios { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Usuarios>(entity =>
+            {
+                entity.Property(e => e.Ativo).HasDefaultValueSql("((1))");
+
+                entity.Property(e => e.DataAlteracao)
+                    .HasColumnType("datetime")
+                    .HasDefaultValueSql("(getdate())");
+
+                entity.Property(e => e.DataCriacao)
+                    .HasColumnType("datetime")
+                    .HasDefaultValueSql("(getdate())");
+
+                entity.Property(e => e.Email)
+                    .HasMaxLength(50)
+                    .IsFixedLength();
+
+                entity.Property(e => e.Login)
+                    .HasMaxLength(50)
+                    .IsFixedLength();
+
+                entity.Property(e => e.Nome)
+                    .IsRequired()
+                    .HasMaxLength(100)
+                    .IsFixedLength();
+
+                entity.Property(e => e.Senha)
+                    .HasMaxLength(50)
+                    .IsFixedLength();
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ ProjetFinalWebApiNetCore/ProjetFinalWebApiNetCore/Models/Usuarios.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace ProjetFinalWebApiNetCore.Models
+{
+    public partial class Usuarios
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Login { get; set; }
+        public string Senha { get; set; }
+        public string Email { get; set; }
+        public bool? Ativo { get; set; }
+        public DateTime? DataCriacao { get; set; }
+        public DateTime? DataAlteracao { get; set; }
+    }
+}
